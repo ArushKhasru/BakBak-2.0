63125683d1bffe3c0d8d7d5ffdeaed6d6e708cad
Arush Khasru <ak3949235@gmail.com>
2026-02-08 21:47:40 +0530

    chat-implemented

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -17,8 +17,23 @@ export async function POST(request) {
   const token = serverClient.createToken(user.data.id);
   console.log("A NEW USER HAS BEEN CREATED", token);
   const client = await clerkClient();
+  await serverClient.upsertUser({id: user.data.id})
+
   await client.users.updateUser(user.data.id, {
     publicMetadata: { token: token },
   });
+
+  //Give acess to this user to all channel
+  const slugs = ["python-new", "cryptography-new", "ui-design-new", "react-nextjs-new", "cybersecurity-new"];
+  await Promise.all(slugs.map(async (item) => {
+     const channel = serverClient.channel('messaging', item, {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: capitalize(item) + "Channel",
+      created_by_id:user.data.id,
+    });
+    await channel.create();
+    await channel.addMembers([user.data.id]);
+  }))
+
   return Response.json({ projectName: 'Next.js' })
 }
